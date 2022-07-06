c2f750c35d9932a685892730cb4ee8f7f43f906a
AmandinChyba <achybarabeendran@mines.edu>
2022-07-06 12:53:47 -0400

    Revised code for part 2a
    added the ability to make multiple runs and average them so that we
    can approximate the expectation.

--- Background_Learning_Questions/q2.py
+++ Background_Learning_Questions/q2.py
@@ -4,28 +4,43 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
+def computeW(n, m, dt, dx):
+    # part 1
+    key = jax.random.PRNGKey(int(time.time()))
+    X = jax.random.choice(key, jnp.array([1,-1]), shape=(m,n))
+
+    W = jnp.zeros(shape=(m,1))
+    W = jnp.append(W, X, axis=1)
+    W = jnp.cumsum(W, axis=1)
+
+    # part 2a
+    #a = jax.random.uniform(key, minval=10, maxval=100)*(-1)
+    #b = jax.random.uniform(key, minval=10, maxval=100)
+    a = -5
+    b = 5
+    
+    rows, cols = jnp.where(jnp.logical_or(W > b,W < a))
+
+    values, indices = jnp.unique(rows, return_index=True)
+   
+    result = jnp.take(cols, indices)
+
+    result = jnp.mean(result)
+
+    return result
+
 dt = 1
 dx = 1
-n = 1000000
+totalSteps = 100
+totalRuns = 10
 
-# part 1
-key = jax.random.PRNGKey(int(time.time()))
-
-X = jax.random.choice(key, jnp.array([1,-1]), shape=(n,))
-
-W = jnp.zeros(shape=())
-
-W = jnp.append(W, X)
-
-W = jnp.cumsum(W)
-
-# part 2a
-a = jax.random.uniform(key, minval=10, maxval=100)*(-1)
-b = jax.random.uniform(key, minval=10, maxval=100)
-
-print(np.where(np.logical_or(W > b,W < a))[0][0])
+result = computeW(totalSteps, totalRuns, dt, dx)
+print(result)
 
 #plt.plot(jnp.arange(0,len(W)*dt, step=dt), W)
 #plt.xlabel("time")
 #plt.ylabel("W(t)")
 #plt.show()
+
+
+
