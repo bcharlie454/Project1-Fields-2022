710eb2199e1c4bcdd3193786ba69d5517373f6b9
AmandinChyba <achybarabeendran@mines.edu>
2022-07-06 11:46:48 -0400

    added code for part 2a

--- Background_Learning_Questions/q2.py
+++ Background_Learning_Questions/q2.py
@@ -4,10 +4,11 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
-dt = 0.2
+dt = 1
 dx = 1
 n = 1000000
 
+# part 1
 key = jax.random.PRNGKey(int(time.time()))
 
 X = jax.random.choice(key, jnp.array([1,-1]), shape=(n,))
@@ -18,9 +19,13 @@ W = jnp.append(W, X)
 
 W = jnp.cumsum(W)
 
-print(W[-1])
+# part 2a
+a = jax.random.uniform(key, minval=10, maxval=100)*(-1)
+b = jax.random.uniform(key, minval=10, maxval=100)
 
-plt.plot(jnp.arange(0,len(W)*dt, step=dt), W)
-plt.xlabel("time")
-plt.ylabel("W(t)")
-plt.show()
+print(np.where(np.logical_or(W > b,W < a))[0][0])
+
+#plt.plot(jnp.arange(0,len(W)*dt, step=dt), W)
+#plt.xlabel("time")
+#plt.ylabel("W(t)")
+#plt.show()
